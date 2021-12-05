6693d0b11664db3cd54b40e02a80c47c626d997a
Adriel Silva <adriel.silva@AC-LPT-00661.local>
2021-12-05 19:47:27 -0300

    feat: random generation

--- src/com/antSim/Game.java
+++ src/com/antSim/Game.java
@@ -2,6 +2,10 @@ package com.antSim;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
@@ -9,6 +13,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import java.awt.Graphics;
+import java.util.Random;
 
 public class Game extends JPanel implements ActionListener {
   
@@ -16,6 +21,7 @@ public class Game extends JPanel implements ActionListener {
   private int antY;
   private int antSpeed = 16;
   private Image ant;
+  private Map<Integer, ArrayList<Integer>> colony = new HashMap<>();
 
   public Game() {
     initGame();
@@ -26,6 +32,7 @@ public class Game extends JPanel implements ActionListener {
     setFocusable(true);
     setBackground(Color.BLACK);
 
+    createColonyAnts();
     loadImages();
   }
 
@@ -45,6 +52,23 @@ public class Game extends JPanel implements ActionListener {
 
   private void drawImage(Graphics g) {
     g.drawImage(ant, antX, antY, this);
+
+    for (int index = 0; index < colony.size(); index++){
+      ArrayList<Integer> colonyAnt = colony.get(index);
+      int colonyAntX = colonyAnt.get(0);
+      int colonyAntY = colonyAnt.get(1);
+
+      g.drawImage(ant, colonyAntX, colonyAntY, this);
+    }
+  }
+
+  private void createColonyAnts() {
+    int index = 0;
+
+    while (index < 5) {
+      colony.put(index, new ArrayList<Integer>(Arrays.asList(0, 0)));
+      index ++;
+    }
   }
 
   private void loadImages() {
@@ -69,6 +93,16 @@ public class Game extends JPanel implements ActionListener {
       if (key == KeyEvent.VK_LEFT) {
         antX -= antSpeed;
       }  
+
+      for (int index = 0; index < colony.size(); index++){
+        Random rand = new Random();
+        int upperbound = 450;
+
+        int colonyAntX = rand.nextInt(upperbound);
+        int colonyAntY = rand.nextInt(upperbound);
+  
+        colony.put(index, new ArrayList<Integer>(Arrays.asList(colonyAntX, colonyAntY)));
+      }
       
       repaint();
     }
